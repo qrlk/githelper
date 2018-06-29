422bf42f2115ca52e0da6c030498c346decc7669
Fyodor Kurlyuk <hi@qrlk.me>
2018-06-29 16:01:15 +0700

    Загружаем все вложенные в scripts скрипты, перезагружаем при изменении времени модификации файла.

--- moonmarket.lua
+++ moonmarket.lua
@@ -8,17 +8,59 @@ script_author("rubbishman")
 script_description("Загружает скрипты из папки scripts")
 -------------------------------------VAR----------------------------------------
 local lfs = require 'lfs'
+local inspect = require 'inspect'
+prefix = ""
+scriptlist = {}
+autoreload = {}
 -------------------------------------MAIN---------------------------------------
 function main()
   if not isSampfuncsLoaded() or not isSampLoaded() then return end
   while not isSampAvailable() do wait(100) end
-  local path = getWorkingDirectory().."/scripts"
-  for file in lfs.dir(path) do
-    if lfs.attributes(file, "mode") == "file" then print("found file, "..file)
-    elseif lfs.attributes(file, "mode") == "directory" then print("found dir, "..file, " containing:")
-      for l in lfs.dir(path..file) do
-        print("", l)
+  dir(getWorkingDirectory().."\\scripts\\")
+  for key, value in pairs(scriptlist) do
+    print('Загружаю '..value)
+    script.load(value)
+    autoreload[key] = lfs.attributes(value, "modification")
+  end
+  while true do
+    wait(0)
+    for key, value in pairs(scriptlist) do
+      if lfs.attributes(value, "modification") ~= autoreload[key] then
+				local scr = find_script_by_path(value)
+				print('Reloading {346cb2}'..scr.name)
+        scr:reload()
+        autoreload[key] = lfs.attributes(value, "modification")
       end
     end
   end
+end
+-----------------------------------HELPERS--------------------------------------
+--эта функция исследует папку и все вложенные в неё папки, достаёт пути к скриптам lua и luac и заносит в таблицу scriptlist
+function dir(path)
+  lfs.chdir(path)
+  for file in lfs.dir(path) do
+    if file ~= "." and file ~= ".."then
+      if lfs.attributes(file, "mode") == "file" then
+        --print(prefix..file)
+        if file:find(".lua", 1, true) or file:find(".luac", 1, true) then
+          table.insert(scriptlist, lfs.currentdir().."\\"..file)
+        end
+      elseif lfs.attributes(file, "mode") == "directory" then
+        --print(prefix..file, " containing:")
+        --prefix = prefix.."    "
+        dir(lfs.currentdir().."\\"..file)
+      end
+    end
+  end
+  lfs.chdir("..")
+  --prefix = string.sub(prefix, 1, string.len(prefix)-4)
+end
+--ищём скрипт по пути, источник: AutoReboot.lua FYP'a
+function find_script_by_path(path)
+  for _, s in ipairs(script.list()) do
+    if s.path == path then
+      return s
+    end
+  end
+  return nil
 end
