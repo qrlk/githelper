a63d5c5e9b4ca5c24b1d416dd5569a245319abf5
Fyodor Kurlyuk <hi@qrlk.me>
2018-06-29 13:58:49 +0700

    Получаем содержимое папки scripts

--- moonmarket.lua
+++ moonmarket.lua
@@ -5,20 +5,20 @@
 script_name("moonmarket")
 script_version("0")
 script_author("rubbishman")
-script_description("")
+script_description("Загружает скрипты из папки scripts")
 -------------------------------------VAR----------------------------------------
 local lfs = require 'lfs'
 -------------------------------------MAIN---------------------------------------
 function main()
   if not isSampfuncsLoaded() or not isSampLoaded() then return end
   while not isSampAvailable() do wait(100) end
-	local path = getWorkingDirectory()
-  for file in lfs.dir(path.."\\scripts") do
+  local path = getWorkingDirectory().."/scripts"
+  for file in lfs.dir(path) do
     if lfs.attributes(file, "mode") == "file" then print("found file, "..file)
     elseif lfs.attributes(file, "mode") == "directory" then print("found dir, "..file, " containing:")
+      for l in lfs.dir(path..file) do
+        print("", l)
+      end
     end
-  end
-  while true do
-    wait(0)
   end
 end
